50f5b3238f755add1549aa85ce009ff3afcd23b7
TakashiKondou <tkondo@a.phys.nagoya-u.ac.jp>
2017-12-16 21:57:05 +0000

    pyinterface

--- scripts/device/ROS_abs.py
+++ scripts/device/ROS_abs.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 
 import sys
-sys.path.append("/opt/ros/kinetic/lib/python2.7/dist-packages")
 import rospy
 import time
 import threading
-sys.path.append("/home/necst/ros/src/necst/lib")
 import pyinterface
 
 from std_msgs.msg import String 
@@ -14,10 +12,11 @@ from necst.msg import Status_hot_msg
 class abs_controller(object):
     #abs = abs.abs_controller()
     pro = 0x00
-    buff = 0x00
+    buff = [1,0,0,0]
     error = []
 
     position = ''
+    move_position = ''
 
     board_name = 2724
     rsw_id = 0 #test
@@ -35,6 +34,9 @@ class abs_controller(object):
         th = threading.Thread(target = self.pub_status)
         th.setDaemon(True)
         th.start()
+        move_thread = threading.Thread(target=self.move)
+        move_thread.setDaemon(True)
+        move_thread.start()
         return
     
 
@@ -65,7 +67,7 @@ if ret == 0x02:
 
     def get_pos(self):
         ret = self.dio.input_byte('IN1_8')
-        print(ret,ret,ret)
+        print(ret)
         if ret[0] == 0 and ret[1] == 1:#ret == 0x02
             self.position = 'IN'
         elif ret[0] == 1 and ret[1] == 0:#ret == 0x01
@@ -77,25 +79,37 @@ if ret == 0x02:
             return
         return self.position
 
-    def move(self,req):
+    def move_pos(self, req):
+        self.move_position = req.data.upper()
+        return self.move_position
+
+    def move(self):
         print('move start')
-        pos = self.get_pos()
-        print(pos)
-        if pos == req.data:
-            print('hot is already ' + req.data)
-            return
-        if req.data.lower() == 'in':
-            #self.pro = 0x00
-            self.buff = [1,0,0,0] #0x01
-        elif req.data.lower() == 'out':
-            #self.pro = 0x02
-            self.buff = [1,1,0,0] #0x03
-        print(req.data)
-        #self.board_abs.out_byte('FBIDIO_OUT1_8', self.pro)
-        #time.sleep(1)
-        self.dio.output_point(self.buff,'OUT1')
-        time.sleep(5)
-        self.get_pos()
+        while not rospy.is_shutdown():
+            print("hot : ", self.position)
+            if self.move_position == self.position:
+                print('hot is already ' , self.position)
+            elif self.move_position != self.position and self.move_position != "":
+                if self.move_position == 'IN':
+                    self.pro = [0,0,0,0] #0x00
+                    self.buff = [1,0,0,0] #0x01
+                    print("hot move IN")
+                elif self.move_position == 'OUT':
+                    self.pro = [0,1,0,0] #0x02
+                    self.buff = [1,1,0,0] #0x03
+                    print("hot move out")
+                else:
+                    rospy.logerr("Bad command!!")
+                    pass
+                self.dio.output_point(self.pro, 1)
+                self.dio.output_point(self.buff,1)
+            elif self.move_position == "":
+                pass
+            else:
+                rospy.logerr("Bad command!!")
+                pass
+            self.move_position = ""
+            time.sleep(0.5)
         return
 
     def emergency(self,req):
@@ -110,9 +124,8 @@ if ret == 0x02:
 
         while not rospy.is_shutdown():
             pos = self.get_pos()
-            print("####################")
             print(pos)
-            msg.hot_position =pos
+            msg.hot_position = pos
             pub.publish(msg)
             rospy.loginfo(pos)
             time.sleep(0.5)
@@ -141,7 +154,7 @@ if __name__ == '__main__':
     rospy.init_node('abs_controller')
     rospy.loginfo('waiting publish abs')
     abs.start_thread()
-    sub = rospy.Subscriber('hot', String, abs.move)
+    sub = rospy.Subscriber('hot', String, abs.move_pos)
     sub = rospy.Subscriber('emergency', String, abs.emergency)
     rospy.spin()
 """
