86fc0b11c9c9d9adbf20a0dd09f9a2da6fe65619
nanten2 <nanten2alab@gmail.com>
2018-01-13 08:28:39 +0000

    update

--- lib/coord.py
+++ lib/coord.py
@@ -336,7 +336,10 @@ class coord_calc(object):
             '''
 
     def read_kisa_file(self, hosei, num):
-        f = open('/home/necst/ros/src/necst/lib/' + hosei)
+        try:
+            f = open('/home/necst/ros/src/necst/lib/' + hosei)
+        except:
+            f = open('/home/amigos/ros/src/necst/lib/' + hosei)
         line = f.readline()
         kisa = [0]*num
         n = 0
