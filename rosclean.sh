f715d9d3d0d8f25a913d577792b830419937ea40
Kazuki Shiotani <shiotani@a.phys.nagoya-u.ac.jp>
2018-01-17 02:50:54 +0000

    update

--- rosclean.sh
+++ rosclean.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 source /opt/ros/kinetic/setup.bash
 source $HOME/ros/devel/setup.bash
-rosclean check
-rosclean purge -y
+date >>/tmp/rosclean.log 
+rosclean check >>/tmp/rosclean.log 2>>/tmp/rosclean_error.log
+rosclean purge -y >>/tmp/rosclean.log 2>>tmp/rosclean_error.log
 
 
