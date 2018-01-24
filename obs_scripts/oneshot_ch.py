711c134e532d7f9ad0de57cc5dd9083d0fd0f1f2
TakashiKondou <tkondo@a.phys.nagoya-u.ac.jp>
2018-01-24 08:42:21 +0000

    script

--- obs_scripts/oneshot_ch.py
+++ obs_scripts/oneshot_ch.py
@@ -6,7 +6,7 @@
 # Info
 # ----
 
-name = 'rsky_by_spectrometer'
+name = 'oneshot_spectrometer'
 description = 'Get R-Sky data.'
 
 
@@ -41,7 +41,6 @@ import os
 import time
 import numpy
 import matplotlib.pyplot
-#import urllib.request
 import datetime
 
 matplotlib.pyplot.rcParams['font.size'] = 9
@@ -61,43 +60,13 @@ else:
     pass
 
 savedir = "/home/amigos/data/experiment/rsky/"
-'''
-fp = urllib.request.urlopen("http://200.91.8.66/WeatherMonitor/WeatherMenu.html")
-html = fp.readline()
-text = []
-while html:
-    html = html.decode('utf-8')
-    text.append(html)
-    html = fp.readline()
-_list = []
-for i in range(len(text)):
-    try:
-        aa = text[i].split(">")[1].split("<")[0]
-    except:
-        aa = text[i]
-    _list.append(aa)
-fp.close()
-'''
-#cabin_temp = float(_list[35].split()[0])
-try:
-    now = time.time()-12*3600.
-    d = datetime.datetime.utcfromtimestamp(now)
-    data = str(d.year)+str(d.month)+"/"+str(d.year)+str(d.month)+str(d.day)+".nwd"
-    f = open("/home/amigos/data/monitor/"+data,"r")
-    last_data = f.readlines()[-1]
-    f.close()
-    data_list = last_data.strip()
-    data_list = data_list.split(",")
-    data = [0]*18
-    for i in range(len(data_list)):
-        data[i] = float(data_list[i].strip())
-    cabin_temp = data[14]
-except:
-    cabin_temp = 28.5
+
 # Data aquisition
 # ---------------
 
 con = ROS_controller.controller()
+status = con.read_status()
+cabin_temp = status.CabinTemp1
 
 d1_list = []
 d2_list = []
@@ -106,62 +75,24 @@ print('Start experimentation')
 
 print('')
 
-print('R')
-#con.move_hot('in')
-
-time.sleep(1)
-
 print('cabin_temp: %.2f'%(cabin_temp))
 
 print('get spectrum...')
 d = con.oneshot_achilles(exposure=integ)
+#d = {"dfs1":[[100]*16384,0], "dfs2":[[200]*16384,0]}
 d1 = d['dfs1'][0]
 d2 = d['dfs2'][0]
 d1_list.append(d1)
 d2_list.append(d2)    
 
-#print('SKY')
-#con.move_hot('out')
-
-#time.sleep(1)
-
-#print('get spectrum...')
-#d = con.oneshot(exposure=integ)
-#d1 = d['dfs1'][0]
-#d2 = d['dfs2'][0]
-#d1_list.append(d1)
-#d2_list.append(d2)    
-
-
-#con.move_hot('in')
-
 d1_list = numpy.array(d1_list)
 d2_list = numpy.array(d2_list)
-
-
 
 print('save...')
 numpy.save(os.path.join(savedir, '%s_dfs01.npy'%(name)), d1_list)
 numpy.save(os.path.join(savedir, '%s_dfs02.npy'%(name)), d2_list)
 numpy.savetxt(os.path.join(savedir, '%s_temp.txt'%(name)), [cabin_temp])
 
-
-def tsys(dhot, dsky, thot):
-    dhot = numpy.array(dhot)
-    dsky = numpy.array(dsky)
-    
-    y = dhot / dsky
-    tsys = thot / (y - 1.)
-    return tsys
-
-#tsys1 = tsys(d1_list[0], d1_list[1], cabin_temp)
-#tsys2 = tsys(d2_list[0], d2_list[1], cabin_temp)
-
-
-#d1_av = numpy.mean(d1_list[:,500:-500], axis=1)
-#d2_av = numpy.mean(d2_list[:,500:-500], axis=1)
-#tsys1_av = tsys(d1_av[0], d1_av[1], cabin_temp)
-#tsys2_av = tsys(d2_av[0], d2_av[1], cabin_temp)
 
 x = numpy.linspace(0, 16384, len(d1_list[0]))
 
@@ -173,8 +104,6 @@ ax22 = ax21.twinx()
 
 ax11.plot(x, d1_list[0], 'r-')
 ax21.plot(x, d2_list[0], 'r-')
-#ax11.plot(x, d1_list[1], 'b-')
-#ax21.plot(x, d2_list[1], 'b-')
 
 ax11.set_yscale('log')
 ax21.set_yscale('log')
@@ -194,8 +123,6 @@ ax22.set_ylim(0, 200)
 ax12.set_ylabel('Tsys (K)')
 ax22.set_ylabel('Tsys (K)')
 
-#ax11.text(0.05, 0.9, 'Tsys = %.1f'%(tsys1_av), transform=ax11.transAxes)
-#ax21.text(0.05, 0.9, 'Tsys = %.1f'%(tsys2_av), transform=ax21.transAxes)
 
 fig.suptitle('%s : %s,  integ = %.2f'%(name, timestamp, integ))
 fig.savefig(os.path.join(savedir, '%s_%s.png'%(name, timestamp)))
