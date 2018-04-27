041700336ac08b48e25dba9522edca90088be972
TakashiKondou <tkondo@a.phys.nagoya-u.ac.jp>
2018-04-27 02:25:20 +0000

    otf_correction

--- scripts/device/ROS_azel_list.py
+++ scripts/device/ROS_azel_list.py
@@ -26,7 +26,8 @@ class azel_list(object):
         self.sub = rospy.Subscriber("wc_list", List_coord_msg, self.receive_list, queue_size=1)
         self.sub = rospy.Subscriber("status_weather", Status_weather_msg, self.receive_weather, queue_size=1)
         self.pub = rospy.Publisher("list_azel", List_coord_msg, queue_size=1)
-        self.stop = rospy.Publisher("move_stop", Bool, queue_size=1)
+        #self.stop = rospy.Publisher("move_stop", Bool, queue_size=1)
+        self.stop = rospy.Subscriber("move_stop", Bool, self.stop, queue_size=1)
         self.calc = calc_coord.azel_calc()
         pass
 
@@ -37,17 +38,18 @@ class azel_list(object):
         if req.timestamp < self.start_time:
             print("receive_old_list...")
         else:
-            pass
-        if req == self.old_list:
-            pass
-        else:
-            self.stop_flag = True
-            self.stop.publish(True)
-            time.sleep(0.5)
-            self.stop_flag = False
-            self.stop.publish(False)
             self.param = req
-            self.old_list = req
+            pass
+        #if req == self.old_list:
+            #pass
+        #else:
+            #self.stop_flag = True
+            #self.stop.publish(True)
+            #time.sleep(0.5)
+            #self.stop_flag = False
+            #self.stop.publish(False)
+            #self.param = req
+            #self.old_list = req
         return
 
     
@@ -58,8 +60,13 @@ class azel_list(object):
             time.sleep(0.1)
         print("start_calclation!!")
         loop = 0
+        param = self.param        
         while not rospy.is_shutdown():
-            if loop == 0:
+            if not self.param:
+                time.sleep(1.)
+                continue
+            if param != self.param:
+                loop = 0
                 param = self.param
             else:
                 pass
@@ -75,18 +82,18 @@ class azel_list(object):
                 x_list2 = [param.x_list[0]+dx*(i+loop*10) for i in range(10)]
                 y_list2 = [param.y_list[0]+dy*(i+loop*10) for i in range(10)]
                 time_list2 = [param.time_list[0]+dt*(i+loop*10) for i in range(10)]
-                for i in range(1,11):
-                    if param.time_list[1]< time_list2[-i]:
-                        del x_list2[-i]
-                        del y_list2[-i]
-                        del time_list2[-i]
+                for i in range(10):
+                    if param.time_list[1]< time_list2[-1]:
+                        del x_list2[-1]
+                        del y_list2[-1]
+                        del time_list2[-1]
                         self.stop_flag = True
                     else:
                         break
                 time_list3 = [datetime.fromtimestamp(time_list2[i]) for i in range(len(time_list2))]
                 astro_time = Time(time_list3)
                 ret = self.calc.coordinate_calc(x_list2, y_list2, astro_time,
-                                                param.coord, param.off_az, param.off_el,
+                                                param.coord, param.off_az, param.off_el, 
                                                 param.hosei, param.lamda, self.weather.press,
                                                 self.weather.out_temp, self.weather.out_humi, param.limit)
                 msg.x_list = ret[0]
@@ -95,14 +102,18 @@ class azel_list(object):
                 msg.from_node =node_name
                 msg.timestamp = time.time()
                 self.pub.publish(msg)
-                #print(msg)
+                print(msg)
                 loop += 1
             else:                
                 loop = 0
+                self.param = ""
                 pass
             time.sleep(0.1)
         return
 
+    def stop(self,req):
+        self.stop_flag = req.data
+    
 if __name__ == "__main__":
     rospy.init_node(node_name)
     azel = azel_list()
