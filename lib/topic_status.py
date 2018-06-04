1a6ae0bac17a1c8e0a8d17939b69f42f0991f42d
TakashiKondou <tkondo@a.phys.nagoya-u.ac.jp>
2018-06-04 08:59:15 +0000

    plus

---
+++ lib/topic_status.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import time
+import rospy
+from necst.msg import Status_node_msg
+
+pub = rospy.Publisher("status_topic", Status_node_msg, queue_size=1)
+
+#import os
+#node_name = os.path.basename(__file__)
+#node_name = node_name.split(".")[0]
+
+def deco(node_name=""):
+    def _deco(func):
+        import functools
+        @functools.wraps(func)
+        def wrapper(*args,**kwargs):
+            pub.publish(True, "start", func.__name__, node_name, time.time())
+            time.sleep(0.1)
+            try:
+                func(*args,**kwargs)
+            except Exception as e:
+                pub.publish(False, str(e), func.__name__, node_name, time.time())
+                time.sleep(0.1)
+                pass
+            pub.publish(False, "end", func.__name__, node_name, time.time())
+        return wrapper
+    return _deco
+
