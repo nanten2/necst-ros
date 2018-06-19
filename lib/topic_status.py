f4db6522f6cd1dbc2859784a20605e142d461f62
TakashiKondou <tkondo@a.phys.nagoya-u.ac.jp>
2018-06-19 09:21:31 +0000

    correction

--- lib/topic_status.py
+++ lib/topic_status.py
@@ -5,19 +5,17 @@ import rospy
 from necst.msg import Status_node_msg
 
 pub = rospy.Publisher("status_topic", Status_node_msg, queue_size=1)
-name = ""
+
 #import os
 #node_name = os.path.basename(__file__)
 #node_name = node_name.split(".")[0]
 
 def deco(node_name=""):
-    global name
     def _deco(func):
         import functools
         @functools.wraps(func)
         def wrapper(*args,**kwargs):
             pub.publish(True, "start", func.__name__, node_name, time.time())
-            name = node_name
             time.sleep(0.1)
             try:
                 func(*args,**kwargs)
