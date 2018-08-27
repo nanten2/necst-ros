e324c7a0e970ea304c69b866d772dc2e7a7bc755
TakashiKondou <tkondo@a.phys.nagoya-u.ac.jp>
2018-08-27 09:06:41 +0000

    script

--- simulator/device/S_ROS_oneshot_achilles.py
+++ simulator/device/S_ROS_oneshot_achilles.py
@@ -8,6 +8,8 @@ sys.path.append("/home/amigos/ros/src/necst/lib")
 import achilles
 import numpy as np
 import time
+from necst.srv import ac240_srv
+from necst.srv import ac240_srvResponse
 
 def oneshot(req):
     print("start oneshot")
@@ -16,35 +18,12 @@ def oneshot(req):
     #data = dfs.oneshot(req.repeat, req.exposure, req.stime)
     data = [[10000.0+np.random.randint(1000)]*int(req.repeat),
             [20000.0+np.random.randint(1000)]*int(req.repeat)]
-    tv = time.time()
-    mjd = tv/24./3600. + 40587.0 # 40587.0 = MJD0
-    while mjd < req.stime:
-        tv = time.time()
-        mjd = tv/24./3600. + 40587.0 # 40587.0 = MJD0            
-        print("mjd,stime", mjd, req.stime)
-        print("wait start")
-        time.sleep(0.01)
     time.sleep(req.exposure*req.repeat)
-    dir_name = "/home/amigos/data/experiment/achilles/" + req.day + "/"
-    if not os.path.exists(dir_name):
-        os.makedirs(dir_name)
-    f1 = open(dir_name+req.day + "_1.txt", "w")
-    print(data[0])
-    f1.write(str(data[0]))
-    f1.close()
-    f2 =  open(dir_name+req.day + "_2.txt", "w")
-    print(data[1])
-    f2.write(str(data[1]))
-    f2.close()
-    f3 =  open(dir_name+req.day + "_fin.txt", "w")
-    f3.write("fin")
-    f3.close()
     print("fin")
-    return
-
+    return ac240_srvResponse(data[0], data[1])
 
 if __name__ == "__main__":
     rospy.init_node("achilles")
     print("start\n")
-    sub = rospy.Subscriber("achilles", Achilles_msg, oneshot)
+    sub = rospy.Service("ac240",ac240_srv, oneshot)
     rospy.spin()
