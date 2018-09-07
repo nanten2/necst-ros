b2663df6ba317531b0634b6394b766e90528ffb7
TakashiKondou <tkondo@a.phys.nagoya-u.ac.jp>
2018-09-07 01:58:29 +0000

    remove

--- lib/pyinterface/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-os.path.join('/home/amigos/ros/src/necst/lib/pyinterface/')
-from . import gpg2000
-create_gpg2000 = gpg2000.gpg2000
-from . import gpg6204
-create_gpg6204 = gpg6204.gpg6204
-from . import gpg7204
-create_gpg7204 = gpg7204.gpg7204
-
