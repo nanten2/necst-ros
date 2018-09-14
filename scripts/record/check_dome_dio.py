68c61551f1a2381f1028bc3e13965a3927c6acca
TakashiKondou <tkondo@a.phys.nagoya-u.ac.jp>
2018-09-14 09:30:33 +0000

    dio

--- scripts/record/check_dome_dio.py
+++ scripts/record/check_dome_dio.py
@@ -1,21 +1,32 @@
+#!/usr/bin/env python3
+
 import pyinterface
 from datetime import datetime as dt
 import time
+import rospy
+from necst.msg import Float64_list_msg
 
 dio = pyinterface.open(2724, 2)
+node_name = "dome_dio"
 
 
-print("check start\n")
-while True:
-    now = dt.utcnow()
-    date = now.strftime("UTC %Y/%m/%d %H:%M:%S")
-    parameter = dio.input_point(2, 6)
-    print(date, " ", parameter)    
+def check_dio():
+    while not rospy.is_shutdown():
+        now = dt.utcnow()
+        date = now.strftime("[UTC] %Y/%m/%d %H:%M:%S")
+        parameter = dio.input_point(2, 6)
+        print(date, " ", parameter)    
+        
+        msg = Float64_list_msg()
+        msg.data = parameter
+        msg.from_node = node_name
+        msg.timestamp = time.time()
+        pub.publish(msg)
+        time.sleep(1.)
+    return
 
-    f = open("antenna_dio_data.txt", "a")
-    f.write(date)
-    f.write(" ")
-    f.write(str(parameter))
-    f.write("\n")
-    f.close()
-    time.sleep(1.)
+if __name__ == "__main__":
+    print("check start\n")
+    rospy.init_node(node_name)
+    pub = rospy.Publisher(node_name, Float64_list_msg, queue_size = 1)
+    check_dio()
