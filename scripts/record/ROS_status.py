b71820b4b9b6c902525a224ee1826516dea1e337
Kazuki Shiotani <shiotani@a.phys.nagoya-u.ac.jp>
2018-10-05 17:16:58 +0000

    fix

--- scripts/record/ROS_status.py
+++ scripts/record/ROS_status.py
@@ -243,7 +243,7 @@ class status_main(object):
             lst_ss = "{0:02d}".format(lst_ss)
             log = "telescope: %s %s %s %s %s %5.0f %6.1f %s:%s:%s %5.2f %5.2f  dome: door %s  membrane: %s %s %5.2f HOT :%s M4 :%s" %(drive[0],drive[1], drive[2], drive[3], antenna_status, mjd, secofday, lst_hh, lst_mm, lst_ss, enc_az, enc_el, doom_door, memb_status, remote_status, dome_enc, hot_position, m4_position)
             log_debug = "telescope: %s %s %s %s %s %5.0f %6.1f %s:%s:%s %5.2f %5.2f %5.2f %5.2f dome: door %s  membrane: %s %s %5.2f HOT :%s M4 :%s M2 :%s" %(drive[0],drive[1], drive[2], drive[3], antenna_status, mjd, secofday, lst_hh, lst_mm, lst_ss, enc_az, enc_el, command_az, command_el, doom_door, memb_status, remote_status, dome_enc, hot_position, m4_position, m2_position)
-            f.write(log + "\n")
+            f.write(log_debug + "\n")
             
             print(log_debug)
             if self.param8["error_msg"]:
