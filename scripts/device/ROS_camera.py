665d6fd6ca04a047cb10344fe871c49949f775e6
okuda <s.okuda@a.phys.nagoya-u.ac.jp>
2018-11-22 08:02:11 +0000

    camera tool

--- scripts/device/ROS_camera.py
+++ scripts/device/ROS_camera.py
@@ -1,4 +1,4 @@
-#!usr/bin/env python3
+#!usr/bin/env python2
 
 import rospy
 import time
@@ -6,63 +6,72 @@ import threading
 import os
 import glob
 import sys
+#sys.path.append('/home/amigos')
 sys.path.append("/opt/ros/kinetic/lib/python2.7/dist-packages")
 sys.path.append("/home/amigos/ros/src/necst/lib")
+sys.path.append('/home/amigos/Pictures/capture')
 import cv2
 import numpy
 import camera
-
+import rospkg
+capture = sys.argv[1]
 from PIL import Image
 im = Image
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
 from necst.msg import oneshot_msg
 
-class camera_controller(object):
+class cam_controller(object):
     filename = ''
+    flag = False
 
     def __init__(self):
-        self.DLSR = camera.controller()
-        seld.DLSR.detect_camera()
-        self.DLSR.set_whitebalance(white='SKY')
-        self.DLSR.set_crop(crop='1.3x')
+        #self.DLSR = camera.controller()
+        #self.DLSR.detect_camera()
+        #self.DLSR.set_whitebalance(white='SKY')
+        #self.DLSR.set_crop(crop='1.3x')
+        print('ready')
         pass
-
+    
     def start_thread(self):
         th = threading.Thread(target=self.pub_image)
         th.setDaemon(True)
         th.start()
         return
-
+    
     def take_picture(self, req):
+        
+        print('start ROS_camera.py')
         self.filename = str(req.time)+'.jpg'
-        while True:
-            if not self.filename == '':
-                break
-            #print('please filename!!')
-            self.DLSR.shutter_download(filename=self.filename)
+        if os.path.exists('/home/amigos/Pictures/capture/'+self.filename) == True:
+            pass
+        else:
+            self.DLSR.shutter_download(filename='/home/amigos/Pictures/capture/'+self.filename)
+            print('oneshot!')
             #self.pub_image()
-            #print('publish'+self.filename)
-            return
-
-    def pub_image(self):
-        while True:
-            if os.path.exists(self.filename) == True:
-                break
-            time.sleep(0.1)
-        print("$$$$$$$$$$$$$")
-        img = cv2.imread(self.filename, 1)
-        bridge = CvBridge()
-        pub = rospy.Publisher('Image', Image_msg, queue_size = 100, latch=True)
-
-        while not rospy.is_shutdown():
-            pub.publish(bridge.cv2_to_imgmsg(omg, 'bgr8'))
-            time.sleep(0.5)
+        
         return
 
+    def pub_image(self):
+        
+        while True:
+            if os.path.exists('/home/amigos/Pictures/capture/'+capture) == True:
+                break
+            time.sleep(0.1)
+        if self.flag == True:
+            image_path = '/home/amigos/Pictures/capture/'
+            img = cv2.imread(image_path + capture)
+            bridge = CvBridge()
+            pub = rospy.Publisher('Image', Image, queue_size = 100, latch=True)
+            pub.publish(bridge.cv2_to_imgmsg(img, 'bgr8'))
+            print('publish picture')
+        else:
+            print('not True')
+        return
+    
 if __name__ == '__main__':
-    cam = camera_controller()
+    cam = cam_controller()
     rospy.init_node('camera_controller')
     cam.start_thread()
-    sub = rospy.Subscriber('oneshot', oneshot_msg, cam.shutter)
-    
+    sub = rospy.Subscriber('oneshot', oneshot_msg, cam.take_picture)
+    rospy.spin()
