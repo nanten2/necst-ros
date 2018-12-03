2a9705d5a2b62584882c71ad6e2007832e7013de
TakashiKondou <tkondo@a.phys.nagoya-u.ac.jp>
2018-12-03 01:52:00 +0000

    hosei

--- scripts/device/ROS_hosei_parameter.py
+++ scripts/device/ROS_hosei_parameter.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import time
 import sys
 sys.path.append("/home/amigos/ros/src/necst/lib")
@@ -14,25 +15,27 @@ node_name = "hosei_parameter"
 def _hosei(req):
     global hosei_file
     hosei_file = req.hosei
+    print("no file")
     return
 
 rospy.init_node(node_name)
 rospy.Subscriber("wc_list", List_coord_msg, _hosei, queue_size=1)
 pub = rospy.Publisher("hosei_parameter", String_list_msg, queue_size=1)
-current_file = ""
+latest_time = ""
 while not rospy.is_shutdown():
     if not hosei_file:
-        print("no new hosei file")
         time.sleep(1.)
         continue
     else:
         pass
-    if hosei_file != current_file:
+    
+    current_time = os.path.getatime("/home/amigos/ros/src/necst/lib/"+hosei_file)
+    if latest_time != current_time:
         f = open("/home/amigos/ros/src/necst/lib/"+hosei_file, "r")
         ff = f.readlines()
         parameter = [hosei_file]
         parameter.extend([i.split("\n")[0] for i in ff])
-        current_file = hosei_file
+        latest_time = current_time
     else:
         pass
     
