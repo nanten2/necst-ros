88d0345c30c5d1f222bde4357d2b4846bb2e80e3
nanten2alab <nanten2alab@gmail.com>
2018-12-17 01:50:38 +0000

    encoder

--- scripts/device/ROS_encoder.py
+++ scripts/device/ROS_encoder.py
@@ -101,8 +101,8 @@ class enc_controller(object):
         return
 
     def get_azel(self):
-        cntAz = int(self.dio.get_counter(1))
-        cntEl = int(self.dio.get_counter(2))
+        cntAz = int(self.dio.get_counter(unsigned=False, ch=1))
+        cntEl = int(self.dio.get_counter(unsigned=False, ch=2))
         #now = dt.utcnow()
         #_utc = now.strftime("%Y-%m-%d %H:%M:%S")
         #print(cntAz)
