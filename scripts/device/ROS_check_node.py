24add8464fb7b0fc8ce186f5f6e55111f5f3a2b1
TakashiKondou <tkondo@a.phys.nagoya-u.ac.jp>
2018-12-18 04:39:30 +0000

    correct

--- scripts/device/ROS_check_node.py
+++ scripts/device/ROS_check_node.py
@@ -3,6 +3,8 @@
 import time
 import rospy
 from necst.msg import Status_node_msg
+from necst.msg import String_necst
+import json
 
 node_name = "check_node"
 topic_dict = {}
@@ -13,18 +15,18 @@ def _topic(req):
     return
 
 def pub_topic():
-    msg = Status_node_msg()
+    msg = String_necst()
     while not rospy.is_shutdown():
         ctopic = topic_dict
-        for i in list(ctopic):
-            msg.from_node = i
-            msg.active = ctopic[i]
-            pub.publish(msg)
-            time.sleep(0.1)
+        dump = json.dumps(ctopic)
+        msg.from_node = node_name
+        msg.timestamp = time.time()
+        msg.data = dump
+        pub.publish(msg)
         time.sleep(1)
             
 
 rospy.init_node(node_name)
 sub = rospy.Subscriber("status_topic", Status_node_msg, _topic, queue_size=1)
-pub = rospy.Publisher("check_node", Status_node_msg, queue_size=1)
+pub = rospy.Publisher("check_node", String_necst, queue_size=1)
 pub_topic()
