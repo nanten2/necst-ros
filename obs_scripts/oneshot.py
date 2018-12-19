02ffa262c0856b1da50f8981b47cf3dae02e1833
okuda <s.okuda@a.phys.nagoya-u.ac.jp>
2018-12-19 05:05:49 +0000

    camera update 12/19 -2

--- obs_scripts/oneshot.py
+++ obs_scripts/oneshot.py
@@ -135,6 +135,7 @@ if not filename:
 dirname = time.strftime("%Y%m%d")
 if not os.path.exists(dirname):
     os.makedirs(dirname)
+    
 one.oneshot(filename, dirname)
 #ccd.oneshot(dirname, filename)
 print(dirname, filename)
