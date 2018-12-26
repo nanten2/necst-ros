077f99800a616b82dc7931791bba9363f4f9c5d9
okuda <s.okuda@a.phys.nagoya-u.ac.jp>
2018-12-26 06:46:43 +0000

    camera update 12/26

--- scripts/device/ROS_oneshot.py
+++ scripts/device/ROS_oneshot.py
@@ -6,28 +6,23 @@ sys.path.append("/opt/ros/kinetic/lib/python2.7/dist_packages")
 import time
 import threading
 from necst.msg import oneshot_msg
-#filename = sys.argv[1]
 
 class oneshot(object):
-    time = 0
-    filename = ''
-    dirname = ''
     
     def __init__(self):
-        rospy.init_node('oneshot_con')
+        #rospy.init_node('oneshot_con')
         pass
     
-    def oneshot(self, filename, dirname):
+    def oneshot(self, filename, dirname, shot_mode):
         msg = oneshot_msg()
         msg.filename = filename
         msg.dirname = dirname
+        msg.shot_mode = shot_mode
         pub = rospy.Publisher('oneshot', oneshot_msg, queue_size=1, latch=True)
         time.sleep(0.1)
-        #pub.publish(msg)
         
         for i in range(100):
             pub.publish(msg)
-            #print('oneshot!!')
             time.sleep(0.1)
         return
 
