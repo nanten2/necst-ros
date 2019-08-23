45df4ee2b35e682304c8c5341b9d97523f3a4eae
r_yamada <yamada@a.phys.nagoya-u.ac.jp>
2019-08-23 05:27:16 +0000

    Refatoring

--- obs_scripts/skydip.py
+++ obs_scripts/skydip.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 # coding:utf-8
 
-#必要なライブラリのインポート
+# 必要なライブラリのインポート
 import ROS_controller
 import time
 import math
@@ -13,9 +13,8 @@ import signal
 
 con = ROS_controller.controller()
 con.get_authority()
-#con.dome_open()
 
-#Ctrl+Cで中止するコマンド
+# Ctrl+Cで中止するコマンド
 def handler(num, flame):
     con.move_stop()
     print("!!Ctrl + C!!")
@@ -26,15 +25,9 @@ def handler(num, flame):
 signal.signal(signal.SIGINT, handler)
     
 opt = con.read_status()
-t_hot = opt.CabinTemp1
-#温度の読み込み
-
-#start_elevation = int(input("Please input start elevation" ))
-#elevation_interval = int(input("Please input interval"))
-#number_of_observing_point = int(input("Please input number of observing point"))
 
 #HOT点の観測
-while opt.Current_Hot == 'OUT':
+while not opt.Current_Hot == 'IN':
     opt = con.read_status()
     time.sleep(1)
     con.move_hot('IN')
@@ -51,7 +44,7 @@ d_hot_integ2_temp = sum(d_hot_dfs2[0])
 d_hot_integ1.append(d_hot_integ1_temp)
 d_hot_integ2.append(d_hot_integ1_temp)
 
-while opt.Current_Hot == 'IN':
+while not opt.Current_Hot == 'OUT':
     opt = con.read_status()
     time.sleep(1)
     con.move_hot('OUT')
@@ -68,7 +61,6 @@ z = [80, 70, 60, 45, 30, 25, 20]
 for elevation in z:
     con.onepoint_move(30,elevation)
     con.dome_track()
-    #con.antenna_tracking()
     con.dome_tracking_check()
     print("dome track OK")
     con.antenna_tracking_check()
@@ -82,14 +74,7 @@ for elevation in z:
     d_integ2.append(d_integ_temp2)
     time.sleep(1)    
 
-print(d_hot_integ1)
-print(d_hot_integ2)
-print(d_integ1)
-print(d_integ2)
-
 #解析開始
-
-
 
 #log(p_hot-p_sky)のlistの作成
 d_hot_sky1 = []
@@ -98,17 +83,12 @@ d_hot_sky2 = []
 for i in range(len(d_integ1)):
     d_temp = d_hot_integ1[0]*5-d_integ1[i]
     d_temp2 = math.log(d_temp)
-    print(d_temp2)
     d_hot_sky1 = d_hot_sky1 + [d_temp2]
 
 for i in range(len(d_integ2)):
     d_temp = d_hot_integ2[0]*5-d_integ2[i]
     d_temp2 = math.log(d_temp)
-    print(d_temp2)
     d_hot_sky2 = d_hot_sky2 + [d_temp2]
-
-#for i in range(len(d_integ1)):
-    #d_integ1_temp = d_integ[i]
 
 #seczの計算
 
@@ -140,13 +120,11 @@ axR.set_title('dfs2')
 axR.set_xlabel('secz')
 axR.set_ylabel('Power')
 
-#axR.text(0.1, 0.1, fit_array1[], transform = axL.transAxes)
 axR.text(0.1, 0.1, 'tau = {:.4f}'.format(fit_array1[0]), transform = axL.transAxes)
-
-#axL.text(1.5, 0.1, fit_array2[1], transform = axL.transAxes)
 axR.text(1.4, 0.1, 'tau = {:.4f}'.format(fit_array2[0]) , transform = axL.transAxes)
 
 axL.grid()
 axR.grid()
+plt.tight_layout()
 plt.show()
 
