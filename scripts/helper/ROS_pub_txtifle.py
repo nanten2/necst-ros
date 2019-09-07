f08ffa5ea4297b3ef4f786abb425ad2ee760e787
KazukiShiotani <shiotani@a.phys.nagoya-u.ac.jp>
2019-09-07 15:13:55 +0000

    fix

--- scripts/helper/ROS_pub_txtifle.py
+++ scripts/helper/ROS_pub_txtifle.py
@@ -1,10 +1,8 @@
 import rospy
 from necst.msg import textfile_msg
-from std_msgs.msg import String
 import time
 import os
 
-path = "/home/amigos/ros/src/necst/lib/hosei_230.txt"
 rospy.init_node("text_publisher")
 
 def callback(req):
@@ -13,13 +11,12 @@ def callback(req):
     txt = f.read()
     f.close()
     s.data = txt
-    s.path = "./{}".format(os.path.basename(req.data))
+    s.path = req.path
     print(txt)
     pub.publish(s)
 
-#time.sleep(2)
 pub = rospy.Publisher("text2", textfile_msg, queue_size=10)
-sub = rospy.Subscriber("text1", String, callback, queue_size=1)
+sub = rospy.Subscriber("text1", textfile_msg, callback, queue_size=1)
 s = textfile_msg()
 
 rospy.spin()
