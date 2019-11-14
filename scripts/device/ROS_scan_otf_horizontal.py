daf753cd7a14729bad17b405844da72ea8fb52d0
KazukiShiotani <shiotani@a.phys.nagoya-u.ac.jp>
2019-11-14 02:08:27 +0000

    add

--- scripts/device/ROS_scan_otf_horizontal.py
+++ scripts/device/ROS_scan_otf_horizontal.py
@@ -80,8 +80,13 @@ class worldcoord(object):
                 else:
                     pass
                 """
+                #start_x = -800
+                #end_x = 800
+                #start_y = 300
+                #end_y = 300
+                print("RET", ret)#for check
                 msg.x_list = [ret[0][0] + start_x, ret[0][1] + end_x]
-                msg.y_list = [ret[1][0] + start_x, ret[1][1] + end_y]
+                msg.y_list = [ret[1][0] + command.off_y, ret[1][1] + command.off_y]
                 print("x_list", msg.x_list)
                 print("y_list", msg.y_list)
                 current_time = time.time()
