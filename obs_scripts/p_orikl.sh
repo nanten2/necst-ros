5bedce67822fd93e7b9e4ddb5b426add0fb61cd2
KazukiShiotani <shiotani@a.phys.nagoya-u.ac.jp>
2019-12-19 00:23:08 +0000

    add

--- obs_scripts/p_orikl.sh
+++ obs_scripts/p_orikl.sh
@@ -8,6 +8,6 @@ else
     echo "observation Repeat $1 times"
     for data in `seq $1`
     do
-	`$path/cross_point_xffts2.py --obsfile line_cross_IRC10216.obs`
+	`$path/cross_point_xffts2.py --obsfile line_cross_OriKL_test.obs`
     done
 fi
