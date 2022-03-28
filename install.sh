a9936ed7922876010e67c48b2f4eb21a284a15a5
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-03-28 21:14:22 +0900

    #506 Support pipenv installation

--- install.sh
+++ install.sh
@@ -1,57 +1,27 @@
 #!/bin/bash
 
 
-# Get path to shell run-command file (i.e. ~/.bashrc, ~/.zshrc, etc.)
-IFS='/' read -r -a array <<< "$SHELL"
-rc_path="$HOME/.${array[-1]}rc"
-
-
-# Check if poetry command available or not
-if ! type poetry > /dev/null 2>&1
+if ! type pipenv > /dev/null 2>&1
 then
-    # Install poetry
+    # Install Pipenv
     echo -e "\033[46mInstalling poetry\033[0m"
-    curl -sSL https://install.python-poetry.org | python3 -
-
-    # Set path to poetry command if not done
-    if grep '.poetry/bin' ${rc_path} > /dev/null 2>&1
+    curl https://raw.githubusercontent.com/pypa/pipenv/master/get-pipenv.py | python3
+    if type pipenv > /dev/null 2>&1
     then
-        echo -e "\033[46mPoetry path has already been set\033[0m"
+        echo -e "\033[46;1mPipenv successfully installed\033[0m"
     else
-        echo -e "\033[46mSetting path in ${rc_path}\033[0m"
-        echo '' >> ${rc_path}
-        echo '# poetry' >> ${rc_path}
-        echo 'export PATH="$HOME/.poetry/bin:$PATH"' >> ${rc_path}
-        echo -e "\033[46;1mPoetry successfully installed\033[0m"
-        export PATH="$HOME/.poetry/bin:$PATH"
-        echo -e "\033[46mRestart the shell to use poetry\033[0m"
+        echo -e "\033[41;1mInstallation failed\033[0m"
+        exit 1
     fi
 fi
-
 
 # Change-directory to the current package
 # *In case this script is executed from other directory, pyproject.toml won't be found
 package_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${package_dir}
 
-
-# To manage global python environment
-poetry config virtualenvs.create false --local
-
-
-# If the package doesn't contain pyproject.toml, create it
-if ! poetry check > /dev/null 2>&1
-then
-    echo -e "\033[46mInitialize virtual environment\033[0m"
-    poetry init
-    echo -e "\033[46mVirtual environment created\033[0m"
-else
-    echo -e "\033[46mValid pyproject.toml exists\033[0m"
-fi
-
-
-# Try poetry install command
-if poetry install
+# Try pipenv sync command
+if pipenv sync --system --dev
 then
     echo -e "\033[46;1m===========================================\033[0m"
     echo -e "\033[46;1m= Successfully installed the dependencies =\033[0m"
@@ -61,20 +31,83 @@ else
     echo -e "\033[46;1m= Installation failed, trying update =\033[0m"
     echo -e "\033[46;1m======================================\033[0m"
 
-    # If poetry install failed, run poetry update
-    if poetry update
+    # If pipenv sync failed, run pipenv update (= pipenv lock && pipenv sync)
+    if pipenv lock && pipenv sync --system --dev
     then
         echo -e "\033[46;1m=========================================\033[0m"
         echo -e "\033[46;1m= Successfully updated the dependencies =\033[0m"
         echo -e "\033[46;1m=========================================\033[0m"
     else
-        echo -e "\033[46;1m=========================================\033[0m"
-        echo -e "\033[46;1m= poetry failed to resolve dependencies =\033[0m"
-        echo -e "\033[46;1m=========================================\033[0m"
+        echo -e "\033[41;1m=========================================\033[0m"
+        echo -e "\033[41;1m= poetry failed to resolve dependencies =\033[0m"
+        echo -e "\033[41;1m=========================================\033[0m"
     fi
 fi
 
 
-# For safety
-poetry config virtualenvs.create true --local
+# # Check if poetry command available or not
+# if ! type poetry > /dev/null 2>&1
+# then
+#     # Install poetry
+#     echo -e "\033[46mInstalling poetry\033[0m"
+#     curl -sSL https://install.python-poetry.org | python3 -
+
+#     if type poetry > /dev/null 2>&1
+#     then
+#         echo -e "\033[46;1mPoetry successfully installed\033[0m"
+#     else
+#         echo -e "\033[41;1mInstallation failed\033[0m"
+#         exit 1
+#     fi
+# fi
+
+
+# # Change-directory to the current package
+# # *In case this script is executed from other directory, pyproject.toml won't be found
+# package_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+# cd ${package_dir}
+
+
+# # To manage global python environment
+# poetry config virtualenvs.create false --local
+
+
+# # If the package doesn't contain pyproject.toml, create it
+# if ! poetry check > /dev/null 2>&1
+# then
+#     echo -e "\033[46mInitialize virtual environment\033[0m"
+#     poetry init
+#     echo -e "\033[46mVirtual environment created\033[0m"
+# else
+#     echo -e "\033[46mValid pyproject.toml exists\033[0m"
+# fi
+
+
+# # Try poetry install command
+# if poetry install
+# then
+#     echo -e "\033[46;1m===========================================\033[0m"
+#     echo -e "\033[46;1m= Successfully installed the dependencies =\033[0m"
+#     echo -e "\033[46;1m===========================================\033[0m"
+# else
+#     echo -e "\033[46;1m======================================\033[0m"
+#     echo -e "\033[46;1m= Installation failed, trying update =\033[0m"
+#     echo -e "\033[46;1m======================================\033[0m"
+
+#     # If poetry install failed, run poetry update
+#     if poetry update
+#     then
+#         echo -e "\033[46;1m=========================================\033[0m"
+#         echo -e "\033[46;1m= Successfully updated the dependencies =\033[0m"
+#         echo -e "\033[46;1m=========================================\033[0m"
+#     else
+#         echo -e "\033[41;1m=========================================\033[0m"
+#         echo -e "\033[41;1m= poetry failed to resolve dependencies =\033[0m"
+#         echo -e "\033[41;1m=========================================\033[0m"
+#     fi
+# fi
+
+
+# # For safety
+# poetry config virtualenvs.create true --local
 
