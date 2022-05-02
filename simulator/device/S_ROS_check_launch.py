48f5e7dd5fa17df3044c07108083cc4eb958152a
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-05-02 19:58:56 +0900

    #505 Use XML parser

--- simulator/device/S_ROS_check_launch.py
+++ simulator/device/S_ROS_check_launch.py
@@ -1,40 +1,41 @@
 #!/usr/bin/env python3
 
 import time
-import ast
+import xml.etree.ElementTree as ET
+from pathlib import Path
+
 import rospy
 import rosnode
 from necst.msg import String_list_msg
 
+
 def launch_check():
-    f = open("/home/amigos/ros/src/necst/simulator/launch/simulator.launch","r")
-    _line = f.readlines()
-    f.close()
-    del _line[0]
-    del _line[-1]
-    launch = [ast.literal_eval(i.split()[2].split("=")[1]) for i in _line]
-    return launch
- 
+    launch = ET.parse("/home/amigos/ros/src/necst/simulator/launch/simulator.launch")
+    root = launch.getroot()
+    return [child.attrib["name"] for child in root]
+
+
 def node_check():
     print("start function")
     launch = launch_check()
     flag = False
     while not rospy.is_shutdown():
         names = rosnode.get_node_names()
-        no_alive = [ i for i in launch if not "/"+str(i) in names]
-        if no_alive == [] and flag==True:
+        no_alive = [i for i in launch if not "/" + str(i) in names]
+        if no_alive == [] and flag == True:
             print("all node is move")
-            pub.publish(no_alive, node_name, time.time())            
+            pub.publish(no_alive, node_name, time.time())
             flag = False
             pass
         elif no_alive:
-            #rospy.logfatal(no_alive)
+            # rospy.logfatal(no_alive)
             pub.publish(no_alive, node_name, time.time())
             flag = True
         else:
             pass
-        time.sleep(1.)
-    return 
+        time.sleep(1.0)
+    return
+
 
 node_name = "check_launch"
 rospy.init_node(node_name)
