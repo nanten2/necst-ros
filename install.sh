a766f10c80be13d80fc2239d1bacf59069c3d545
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-05-06 16:29:28 +0900

    #497 Remove pyinterface from dependency
    Since Poetry cannot install Git dependency into global environment, which is caused by misspecification of default target directory of 'git clone'

--- install.sh
+++ install.sh
@@ -1,49 +1,10 @@
 #!/bin/bash
 
 
-# if ! type pipenv > /dev/null 2>&1
-# then
-#     # Install Pipenv
-#     echo -e "\033[46mInstalling pipenv\033[0m"
-#     curl -sSL https://raw.githubusercontent.com/pypa/pipenv/master/get-pipenv.py | python3 -
-#     if type pipenv > /dev/null 2>&1
-#     then
-#         echo -e "\033[46;1mPipenv successfully installed\033[0m"
-#     else
-#         echo -e "\033[41;1mInstallation failed\033[0m"
-#         exit 1
-#     fi
-# fi
-
-# # Change-directory to the current package
-# # *In case this script is executed from other directory, Pipfile won't be found
-# package_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-# cd ${package_dir}
-
-# # Try pipenv sync command
-# if pipenv sync --system --dev
-# then
-#     echo -e "\033[46;1m===========================================\033[0m"
-#     echo -e "\033[46;1m= Successfully installed the dependencies =\033[0m"
-#     echo -e "\033[46;1m===========================================\033[0m"
-# else
-#     echo -e "\033[46;1m======================================\033[0m"
-#     echo -e "\033[46;1m= Installation failed, trying update =\033[0m"
-#     echo -e "\033[46;1m======================================\033[0m"
-
-#     # If pipenv sync failed, run pipenv update (= pipenv lock && pipenv sync)
-#     if pipenv lock && pipenv sync --system --dev
-#     then
-#         echo -e "\033[46;1m=========================================\033[0m"
-#         echo -e "\033[46;1m= Successfully updated the dependencies =\033[0m"
-#         echo -e "\033[46;1m=========================================\033[0m"
-#     else
-#         echo -e "\033[41;1m=========================================\033[0m"
-#         echo -e "\033[41;1m= poetry failed to resolve dependencies =\033[0m"
-#         echo -e "\033[41;1m=========================================\033[0m"
-#     fi
-# fi
-
+# Define installer for additional package Poetry cannot handle
+install_additional_packages(){
+    pip3 install -qU git+https://github.com/ars096/pyinterface2.git; sys_platform == 'linux'
+}
 
 # Check if poetry command available or not
 if ! type poetry > /dev/null 2>&1
@@ -86,6 +47,7 @@ fi
 # Try poetry install command
 if poetry install
 then
+    install_additional_packages()
     echo -e "\033[46;1m===========================================\033[0m"
     echo -e "\033[46;1m= Successfully installed the dependencies =\033[0m"
     echo -e "\033[46;1m===========================================\033[0m"
@@ -97,6 +59,7 @@ else
     # If poetry install failed, run poetry update
     if poetry update
     then
+        install_additional_packages()
         echo -e "\033[46;1m=========================================\033[0m"
         echo -e "\033[46;1m= Successfully updated the dependencies =\033[0m"
         echo -e "\033[46;1m=========================================\033[0m"
