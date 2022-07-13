53a9542ae0c83702463d9657e2c4854e12698cc0
fuuna <t.fuuna@a.phys.nagoya-u.ac.jp>
2022-07-13 23:33:18 +0900

    #525 implement skydip observation script

--- obs_scripts/skydip.py
+++ obs_scripts/skydip.py
@@ -1,131 +1,109 @@
-#! /usr/bin/env python3
-# coding:utf-8
+#!/usr/bin/env python3
 
-# 必要なライブラリのインポート
-import ROS_controller
 import time
-import math
-import matplotlib.pylab as plt
-from scipy.optimize import curve_fit
-import numpy as np
-import sys
-import signal
+from typing import List
 
-con = ROS_controller.controller()
-con.get_authority()
-
-# Ctrl+Cで中止するコマンド
-def handler(num, flame):
-    con.move_stop()
-    print("!!Ctrl + C!!")
-    print("Stop anntena")
-    con.obs_status(active=False)
-    sys.exit()
+from _observation import Observation
 
 
-signal.signal(signal.SIGINT, handler)
+class Skydip(Observation):
+    """Skydip observation.
 
-opt = con.read_status()
+    Parameters
+    ----------
+    integ
+        HOT観測とSKY観測の積分時間
 
-# HOT点の観測
-while not opt.Current_Hot == "IN":
-    opt = con.read_status()
-    time.sleep(1)
-    con.move_hot("IN")
+    """
 
-d_hot_integ1 = []
-d_hot_integ2 = []
-d_hot_raw = con.oneshot_achilles(repeat=1, exposure=1.0, stime=1.0)
-d_hot_dfs1 = d_hot_raw["dfs1"]
-d_hot_dfs2 = d_hot_raw["dfs2"]
+    ObservationType = "SKYDIP"
+    Elevation: List[float] = [80, 70, 60, 45, 30, 25, 20]
 
-d_hot_integ1_temp = sum(d_hot_dfs1[0])
-d_hot_integ2_temp = sum(d_hot_dfs2[0])
+    def __init__(self, *args, integ: float, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.integ = integ
 
-d_hot_integ1.append(d_hot_integ1_temp)
-d_hot_integ2.append(d_hot_integ1_temp)
+    def wait_for_chopper_move(self) -> None:
+        time.sleep(3)
 
-while not opt.Current_Hot == "OUT":
-    opt = con.read_status()
-    time.sleep(1)
-    con.move_hot("OUT")
+    def get_spectra(
+        self,
+        exposure: float,
+        obsmode: str,
+        pt_idx: int,
+    ) -> None:
+        self.ctrl.xffts_publish_flag(
+            scan_num=pt_idx,
+            obs_mode=obsmode,
+        )
+        time.sleep(exposure)
+        self.ctrl.xffts_publish_flag()
 
+    def run_hot_observation(self) -> None:
+        self.log.info("Start HOT observation...")
+        self.ctrl.move_chopper("in")
+        self.wait_for_chopper_move()
 
-opt = con.read_status()
+        self.ctrl.obs_status(active=True, current_position="HOT")
 
+        self.get_spectra(self.integ, "HOT", 99)
 
-# skydip測定の開始
-d_integ1 = []
-d_integ2 = []
-z = [80, 70, 60, 45, 30, 25, 20]
+        self.ctrl.move_chopper("out")
 
-con.dome_track()
-for elevation in z:
-    con.onepoint_move(30, elevation)
-    time.sleep(2)
-    con.dome_tracking_check()
-    print("dome track OK")
-    con.antenna_tracking_check()
-    print("antenna track OK")
-    con.move_stop()
-    d = con.oneshot_achilles(repeat=1, exposure=1.0, stime=1.0)
-    d_dfs1 = d["dfs1"]
-    d_dfs2 = d["dfs2"]
-    d_integ_temp1 = sum(d_dfs1[0])
-    d_integ_temp2 = sum(d_dfs2[0])
-    d_integ1.append(d_integ_temp1)
-    d_integ2.append(d_integ_temp2)
+    def run(self) -> None:
+        """Run the observation."""
+        self.log.info(f"Start {self.ObservationType} observation...")
+        self.ctrl.move_stop()
+        self.ctrl.xffts_publish_flag()
 
-# 解析開始
+        self.ctrl.dome_track()
 
-# log(p_hot-p_sky)のlistの作成
-d_hot_sky1 = []
-d_hot_sky2 = []
+        self.ctrl.obs_status(
+            active=True,
+            obsmode=self.ObservationType,
+            obs_script=__file__,
+            exposure_hot=self.integ,
+            exposure_on=self.integ,
+        )
 
-for i in range(len(d_integ1)):
-    d_temp = d_hot_integ1[0] * 5 - d_integ1[i]
-    d_temp2 = math.log(d_temp)
-    d_hot_sky1 = d_hot_sky1 + [d_temp2]
+        self.run_hot_observation()
 
-for i in range(len(d_integ2)):
-    d_temp = d_hot_integ2[0] * 5 - d_integ2[i]
-    d_temp2 = math.log(d_temp)
-    d_hot_sky2 = d_hot_sky2 + [d_temp2]
+        self.log.info("Start SKY observation...")
+        status = self.con.read_status()
+        current_Az = status.Current_Az
 
-# seczの計算
+        for elevation in self.Elevation:
+            self.ctrl.onepoint_move(current_Az, elevation)
+            self.ctrl.dome_tracking_check()
+            self.log.debug("dome track OK")
+            self.ctrl.antenna_tracking_check()
+            self.log.debug("antenna track OK")
+            self.ctrl.move_stop()
+            self.get_spectra(self.integ, "SKY", elevation)
 
-secz = []
-for i in range(len(z)):
-    secz_temp = (z[i] / 180) * math.pi
-    secz.append(1 / math.cos(secz_temp))
-
-print(d_hot_sky1)
-print(d_hot_sky2)
-# plot
-
-fig, (axL, axR) = plt.subplots(ncols=2, figsize=(10, 4))
-
-# fitting
-fit_array1 = np.polyfit(secz, d_hot_sky1, 1)
-fit_array2 = np.polyfit(secz, d_hot_sky2, 1)
+        self.ctrl.move_stop()
+        self.log.info(f"{self.ObservationType.upper()} observation finished.")
 
 
-axL.plot(secz, np.poly1d(fit_array1)(secz), label="dfs1")
-axL.plot(secz, d_hot_sky1, "o")
-axL.set_title("dfs1")
-axL.set_xlabel("secz")
-axL.set_ylabel("Power")
+if __name__ == "__main__":
+    import argparse
 
-axR.plot(secz, np.poly1d(fit_array2)(secz), label="dfs1")
-axR.plot(secz, d_hot_sky2, "o")
-axR.set_title("dfs2")
-axR.set_xlabel("secz")
-axR.set_ylabel("Power")
+    p = argparse.ArgumentParser(description="Skydip observation")
+    p.add_argument(
+        "--integ", type=float, default=1.0, help="Integration time (sec). Default = 1.0"
+    )
+    p.add_argument(
+        "-v",
+        "--verbose",
+        action="count",
+        default=0,
+        help=(
+            "Verbosity level of log messages appear on terminal."
+            "To show all messages, use '-vvv'."
+        ),
+    )
+    args = p.parse_args()
 
-axR.text(0.1, 0.1, "tau = {:.4f}".format(fit_array1[0]), transform=axL.transAxes)
-axR.text(1.4, 0.1, "tau = {:.4f}".format(fit_array2[0]), transform=axL.transAxes)
-
-axL.grid()
-axR.grid()
-plt.tight_layout()
-plt.show()
+    observer = Skydip(integ=args.integ, verbose=20 + args.verbose * 10)
+    observer.run()
+    observer.ctrl.release_authority()
